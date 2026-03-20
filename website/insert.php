88e71f2fc5b6ebeb705bb5190859ca93fc4eee20
Anuragg26 <anuragg.panwarr@gmail.com>
2026-03-20 17:46:58 +0100

    Update insert.php
    Implemented backend logic to insert user data into MySQL RDS database using PHP.

--- website/insert.php
+++ website/insert.php
@@ -1,1 +1,24 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+<?php
+$conn = new mysqli("mydb.cxqoii4oyf61.eu-north-1.rds.amazonaws.com", "admin", "Anurag12345", "myapp");
 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$gender = $_POST['gender'];
+$country_code = $_POST['country_code'];
+$phone = $_POST['phone'];
+
+$sql = "INSERT INTO users (name, email, gender, country_code, phone)
+VALUES ('$name', '$email', '$gender', '$country_code', '$phone')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+?>
