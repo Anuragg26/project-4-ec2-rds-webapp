855ba5f94d3671cde810eb4126f03fc57ddb3619
Anuragg26 <anuragg.panwarr@gmail.com>
2026-03-20 17:44:06 +0100

    Update delete.php
    Implemented delete feature to remove records from database using user ID.

--- website/delete.php
+++ website/delete.php
@@ -1,1 +1,20 @@
+<?php
+$conn = new mysqli(
+"mydb.cxqoii4oyf61.eu-north-1.rds.amazonaws.com",
+"admin",
+"Anurag12345",
+"myapp"
+);
 
+$id = $_GET['id'];
+
+$sql = "DELETE FROM users WHERE id=$id";
+
+if ($conn->query($sql) === TRUE) {
+    header("Location: view.php");
+} else {
+    echo "Error deleting record";
+}
+
+$conn->close();
+?>
